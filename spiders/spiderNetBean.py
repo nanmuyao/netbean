606ed7a089dd9e1727f6e158badb2826d99977eb
nanmuyao <1558249222han@gmail.com>
2018-05-22 15:24:04 +0800

    整理代码
    整理代码

--- spiders/spiderNetBean.py
+++ spiders/spiderNetBean.py
@@ -1,74 +1,61 @@
-# # # -*- coding: utf-8 -*-
-# #
-# # from selenium import webdriver
+# # -*- coding: utf-8 -*-
+#
+# from selenium import webdriver
 # # from scrapy.selector import Selector
-# # import time
-# #
-# # # 设置chromedriver不加载图片
-# # chrome_options = webdriver.ChromeOptions()
-# # # prefs = {"profile.managed_default_content_settings.images": 2}
-# # # chrome_options.add_experimental_option("prefs", prefs)
-# # # chrome_options.add_argument("--headless")
-# #
-# # # browser = webdriver.Chrome(executable_path="D:\workspace_p\pythondoc\chromedriver.exe", chrome_options=chrome_options)
-# # browser = webdriver.Chrome(executable_path="D:\workspace_p\pythondoc\chromedriver.exe")
-# #
-# # # browser.get("https://www.zhihu.com/#signin")
-# # # time.sleep(15)
-# # # browser.find_element_by_css_selector(".view-signin input[name='account']").send_keys("222222")
-# # # browser.find_element_by_css_selector(".view-signin input[name='password']").send_keys("admin125")
-# # # browser.find_element_by_css_selector(".view-signin button.sign-button").click()
-# #
-# # # browser.get("http://weibo.com/hoopchina?refer_flag=1001030102_")
-# # browser.get("https://music.163.com/song?id=449818741")
-# # time.sleep(5)
-# # print("休眠结束")
-# # #切换到contentFrame
-# # browser.switch_to.frame("contentFrame")
-# #
-# #
-# # print(browser.page_source)
-# #
-# # time.sleep(2)
-# #
-# #
-# #
-# # # 下一页按钮
-# # browser.find_element_by_class_name('znxt').click()
-# #
-# # time.sleep(2)
-# #
-# # browser.find_element_by_class_name('znxt').click()
-# #
-# # print(browser.find_element_by_id('comment-box').text)
-# #
-# # # time.sleep(15)
-# # # browser.find_element_by_css_selector("#loginname").send_keys("15114626480")
-# # # browser.find_element_by_css_selector(".info_list.password input[node-type='password']").send_keys("111111")
-# # # browser.find_element_by_css_selector(".info_list.login_btn a[node-type='submitBtn']").click()
-# #
-# # # browser.find_element_by_css_selector("#loginname").send_keys("liyao198705@sina.com")
-# # # browser.find_element_by_css_selector(".info_list.password input[node-type='password']").send_keys("da_ge_da")
-# # # browser.find_element_by_css_selector(".info_list.login_btn a[node-type='submitBtn']").click()
-# #
-# #
-# #
-# # # for i in range(3):
-# # #     browser.execute_script("window.scrollTo(0, document.body.scrollHeight); var lenOfPage=document.body.scrollHeight; return lenOfPage;")
-# # #     time.sleep(3)
-# # # t_selector = Selector(text=browser.page_source)
-# # # print (t_selector.css(".tm-promo-price .tm-price::text").extract())
-# #
-# #
-# # # browser.quit()
-#
-#
-#
 # import time
-# fo = open("comments.txt", "w+")
-# fo.write("abcdef123")
-# fo.flush()
-# fo.write("abcdef456")
-# time.sleep(5)
-# fo.close()
 #
+# print("startTime", time.time())
+# # 设置chromedriver不加载图片
+# chrome_options = webdriver.ChromeOptions()
+# #设置游览器不加载图片，节省网络流量提高速度
+# prefs = {"profile.managed_default_content_settings.images": 2}
+# chrome_options.add_experimental_option("prefs", prefs)
+# #新版本chrome支持无界面游览了
+# chrome_options.add_argument("--headless")
+# browser = webdriver.Chrome(executable_path="E:\pythonDoc\chromedriver.exe", chrome_options=chrome_options)
+#
+# # 光年之外
+# browser.get("https://music.163.com/song?id=449818741")
+# time.sleep(3)
+# #原本的page_source中没有评论用到的数据切换到contentFrame
+# browser.switch_to.frame("contentFrame")
+# fo = open("comments.txt", "w+", encoding='utf-8')
+#
+# commentCount = 0
+# def getPageData():
+#     global commentCount
+#     # 解析每一条数据
+#     nameList = browser.find_element_by_id('comment-box').find_elements_by_css_selector(".f-brk")
+#     for content in nameList:
+#         try:
+#             nameContent = content.find_element_by_css_selector('a')
+#             name = nameContent.text
+#             # print(name)
+#             contents = content.text
+#             comment = contents[len(name) + 1:len(contents)] + '\n'
+#             fo.write(comment)
+#             fo.flush()
+#             commentCount += 1
+#         except:
+#             print("出错了怎么回事")
+#
+#
+# #点击下一页按钮
+# def getNextPageData():
+#     browser.find_element_by_class_name('znxt').click()
+#
+#
+# # 先拿10页的数据
+# getPageData()
+# time.sleep(1)
+# for index in range(2500):
+#     print("当前抓取的页数" + str(index + 1) + "页")
+#     getNextPageData()
+#     time.sleep(0.7)
+#     getPageData()
+#
+# print("endTime", time.time())
+# fo.close()
+# print("一共抓取到了" + str(commentCount) + "条数据")
+#
+# browser.quit()
